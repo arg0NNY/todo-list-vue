60ab370572d7b689a3ce1a8249c3d833416e091c
Олег Капранов <arg0nny@mail.ru>
2023-03-19 00:57:38 +0700

    Modal close button

--- src/components/general/Modal.vue
+++ src/components/general/Modal.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="modal-container" @mousedown.self="emit('close')">
     <div class="modal">
-      <slot />
+      <button class="modal-close-btn" @click="emit('close')"><IconX/></button>
+      <div class="modal__content">
+        <slot />
+      </div>
     </div>
   </div>
 </template>
@@ -9,6 +12,7 @@
 <script setup>
 import {useScrollLock} from "@vueuse/core"
 import {onBeforeUnmount, onMounted} from "vue"
+import IconX from "@/components/icons/IconX.vue"
 
 const emit = defineEmits(['close'])
 
@@ -28,6 +32,25 @@ onBeforeUnmount(() => isLocked.value = false)
   width: 100%;
   height: 100%;
   background: rgba(0,0,0,.5);
+  z-index: 100000;
+}
+
+.modal-close-btn {
+  flex-shrink: 0;
+  width: 30px;
+  height: 30px;
+  background: none;
+  border: none;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: $background-secondary;
+
+  .icon {
+    width: 20px;
+    height: 20px;
+  }
 }
 
 .modal {
@@ -35,20 +58,29 @@ onBeforeUnmount(() => isLocked.value = false)
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  background: $background-secondary;
-  border-radius: 15px;
-  padding: 25px;
-  width: 700px;
-  box-shadow: 0 67px 27px rgba(0, 0, 0, 0.01), 0 37px 22px rgba(0, 0, 0, 0.05), 0 17px 17px rgba(0, 0, 0, 0.09), 0 4px 9px rgba(0, 0, 0, 0.1), 0 0 0 rgba(0, 0, 0, 0.1);
   max-height: 90vh;
-  overflow-x: hidden;
-  overflow-y: auto;
-  z-index: 100000;
+  width: 700px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+
+  &__content {
+    width: 100%;
+    padding: 25px;
+    overflow-x: hidden;
+    overflow-y: auto;
+    background: $background-secondary;
+    border-radius: 15px;
+    box-shadow: 0 67px 27px rgba(0, 0, 0, 0.01), 0 37px 22px rgba(0, 0, 0, 0.05), 0 17px 17px rgba(0, 0, 0, 0.09), 0 4px 9px rgba(0, 0, 0, 0.1), 0 0 0 rgba(0, 0, 0, 0.1);
+  }
 
   @include mobile {
-    border-radius: 10px;
-    padding: 15px;
     width: calc(100% - 30px);
+
+    &__content {
+      border-radius: 10px;
+      padding: 15px;
+    }
   }
 }
 </style>
